e92734ea6733487647974356a03e971193105e06
Muzietto <marco_faustinelli@yahoo.com>
2015-08-21 09:05:54 +0200

    started implementation

--- public/js/components.js
+++ public/js/components.js
@@ -2,16 +2,79 @@
 module.exports = (function () {
   'use strict'; 
 
-  var sensor = function(pump, level) {
-    return {};
-  };
+  function level(start) {
+    var _level = centify(start) || 0;
+    return {
+      value : function() {
+        return _level/100;
+      },
+      incr : function(val) {
+        _level += centify(val) || 1;
+      },
+      decr : function(val) {
+        _level -= centify(val) || 1;
+      }
+    };
+  }
+  
+  function volume(area, level) {
+    return {
+      value : function() {
+        return decimillifyVolume()/10000;
+      },
+      incr : function(val) {
+        var _val = centify(val) || 1;
+        var _volume = area * 12;
+        level.incr(centify());
+      },
+      decr : function(val) {
+        level.decr(centify());
+      }
+    };
+    
+    function decimillifyVolume() {
+      return centify(area) * centify(level.value());
+    }
+  }
 
-  var pump = function(level, log, waterSensor) {
-    return {};
-  };
+  function centify(val) { return Math.round(val * 100); }
+  function gt(a, b) { return a > b; }
+  function lt(a, b) { return a < b; }
+
+  function _sensor(level, threshold, comparator) {
+    comparator = comparator || gt;
+    return result;
+
+    function result() {
+      return comparator(level.value(), threshold || 0);
+    }
+  }
+  
+  function sensorAbove() {
+    return function(level, threshold) {
+      return _sensor(level, threshold, gt);
+    };
+  }
+  function sensorBelow() {
+    return function(level, threshold) {
+      return _sensor(level, threshold, lt);
+    };
+  }
+
+  function pump(level, sensor) {
+    return {
+      on : function() {
+        return sensor();
+      }
+    };
+  }
 
   return {
-    sensor: sensor,
+    level: level,
+    volume: volume,
+    sensor: sensorAbove(),
+    sensorAbove: sensorAbove(),
+    sensorBelow: sensorBelow(),
     pump: pump
   };
-}());  
+}());
--- spec/js/components.js
+++ spec/js/components.js
@@ -7,7 +7,11 @@ var expect = chai.expect
 chai.use(require('sinon-chai'))
 
 var components = require('../../public/js/components.js');
+var level = components.level;
+var volume = components.volume;
 var sensor = components.sensor;
+var sensorAbove = components.sensorAbove;
+var sensorBelow = components.sensorBelow;
 var pump = components.pump;
 
 describe('component', function() {
@@ -20,9 +24,97 @@ describe('component', function() {
   afterEach(function(){
   });
 
-  describe('pump', function() {
-    it('must be instantiated xxxxx', function() {
-      expect(pump()).to.be.not.equal(pump());
+  describe('level', function() {
+    it('can be created with starting value or with default zero', function() {
+      var l1 = level();
+      expect(l1.value()).to.be.equal(0);
+      var l2 = level(123);
+      expect(l2.value()).to.be.equal(123);
+    });
+    it('can be increased or decreased by 0.01', function() {
+      var ll = level();
+      expect(ll.value()).to.be.equal(0);
+      ll.incr();
+      expect(ll.value()).to.be.equal(0.01);
+      ll.decr();
+      expect(ll.value()).to.be.equal(0);
+    });
+    it('can be increased or decreased by n', function() {
+      var ll = level();
+      expect(ll.value()).to.be.equal(0);
+      ll.incr(12);
+      expect(ll.value()).to.be.equal(12);
+      ll.decr(2);
+      expect(ll.value()).to.be.equal(10);
+    });
+    it('handles max precision 1e-2', function() {
+      var l0 = level(0.012345);
+      expect(l0.value()).to.be.equal(0.01);
+      
+      var l1 = level();
+      expect(l1.value()).to.be.equal(0);
+      l1.incr(12.12345);
+      expect(l1.value()).to.be.equal(12.12);
+      l1.decr(2.0012345);
+      expect(l1.value()).to.be.equal(10.12);
+      
+    });
+  });
+  
+  describe('volume', function() {
+    it('has a base area and a level with precision 1e-2', function() {
+      var v0 = volume(10.0012345,level(5.0012345));
+      expect(v0.value()).to.be.equal(50);
+    });
+    it('can be increased and decreased by 0.0001', function() {
+      var l0 = level(5)
+      var v0 = volume(10, l0);
+      expect(v0.value()).to.be.equal(50);
+      v0.incr();
+      expect(v0.value()).to.be.equal(50.0001);
+      expect(l0.value()).to.be.equal(5);
+      v0.decr();
+      expect(v0.value()).to.be.equal(50);
+      expect(l0.value()).to.be.equal(5);
+    });
+    it('cannot go under zero', function() {
+      
+    });
+  });
+
+  describe('sensor', function() {
+    it('may go off when its level is > threshold', function() {
+      var l0 = level(5);
+      var s0 = sensorAbove(l0,5);
+      expect(s0()).to.be.not.ok;
+      l0.incr();
+      expect(s0()).to.be.ok;
+      l0.decr();
+      expect(s0()).to.be.not.ok;
+
+      var s1 = sensor(l0,5);
+      expect(s1()).to.be.not.ok;
+      l0.incr();
+      expect(s1()).to.be.ok;
+    });
+    it('may go off when its level is < threshold', function() {
+      var l0 = level(5);
+      var s0 = sensorBelow(l0,5);
+      expect(s0()).to.be.not.ok;
+      l0.decr();
+      expect(s0()).to.be.ok;
+    });
+  });
+  
+  describe('pump', function(){
+    it('may be on or off depending on what its sensor senses', function() {
+      var l0 = level();
+      var s0 = sensor(l0)
+      var p0 = pump(l0, s0);
+      expect(p0.on()).to.be.not.ok;
+      
+      l0.incr();
+      expect(p0.on()).to.be.ok;
     });
   });
 
