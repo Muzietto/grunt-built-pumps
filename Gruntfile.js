26d3077f81c2da1ee2dc94dae7adb9a51c316188
Muzietto <marco_faustinelli@yahoo.com>
2015-09-01 17:17:22 +0200

    started implementation of remote deploy

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,35 @@ module.exports = function(grunt){
   grunt.initConfig({
     // used by the changelog task
     pkg: grunt.file.readJSON('package.json'),
+    secret: grunt.file.readJSON('../server_tedesco.json'),
+
+    environments: {
+      options: {
+        local_path: 'build',
+        current_symlink: 'current',
+        deploy_path: '/var/www/vhosts/faustinelli.org/node.faustinelli.org/grunt-built-pumps'
+      },
+  /*    staging: {
+          options: {
+              host: '<%= secret.staging.host %>',
+              username: '<%= secret.staging.username %>',
+              password: '<%= secret.staging.password %>',
+              port: '<%= secret.staging.port %>',
+              debug: true,
+              releases_to_keep: '3'
+          }
+      },*/
+      production: {
+          options: {
+              host: '<%= secret.production.host %>',
+              username: '<%= secret.production.username %>',
+              password: '<%= secret.production.password %>',
+              port: '<%= secret.production.port %>',
+              releases_to_keep: '5',
+              release_subdir: 'node.faustinelli.org/grunt-built-pumps'
+          }
+      }
+    },
 
     // http://fairwaytech.com/2014/01/understanding-grunt-part-2-automated-testing-with-mocha/
     simplemocha: {
@@ -21,6 +50,11 @@ module.exports = function(grunt){
         files: {
           'build/js/bundle.js': 'public/js/**/*.js'
         }
+      },
+      css: {
+        files: {
+          'build/css/bundle.css': 'public/css/**/*.css'
+        }
       }
     },
 
@@ -33,13 +67,12 @@ module.exports = function(grunt){
     },
 
     clean: {
-      js: 'build/js'
+      build: ['build']
     },
 
     jshint: {
-      client: ['public/js/**/*.js', '!public/js/utils/**/*.js'],
-      server: ['server/js/**/*.js'],
-      support: ['public/js/**/*.js']
+      client: ['public/js/**/*.js', '!public/js/utils/**/*.js' ],
+      server: ['server/js/**/*.js']
     },
 
     // --> run 'grunt watch:lint_client'
@@ -80,10 +113,11 @@ module.exports = function(grunt){
   grunt.loadNpmTasks('grunt-simple-mocha');
   grunt.loadNpmTasks('grunt-bump');
   grunt.loadNpmTasks('grunt-conventional-changelog');
+  grunt.loadNpmTasks('grunt-ssh-deploy');
 
   grunt.registerTask('notes', ['bump-only', 'conventionalChangelog', 'bump-commit']);
-  grunt.registerTask('build:debug', 'minimal processing', ['jshint', 'simplemocha:all', 'clean:js', 'concat:js']);
-  grunt.registerTask('build:release', 'Concatenate and minify js files', ['jshint', 'simplemocha:all', 'clean:js', 'concat:js', 'uglify:bundle', 'bump-remote']);
+  grunt.registerTask('build:debug', 'minimal processing', ['jshint', 'simplemocha:all', 'clean:build']);
+  grunt.registerTask('build:release', 'Concatenate and minify js files', ['jshint', 'simplemocha:all', 'clean:build', 'concat:js', 'uglify:bundle',/* 'bump',*/ 'ssh_deploy:production']);
 
   grunt.registerTask('timestamp', function() {
     var options = this.options({
