6a82f5d34133c02610206be60b91c2572ae602ff
Muzietto <marco_faustinelli@yahoo.com>
2015-09-02 15:04:02 +0200

    using the new port

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var users = require('./server/routes/users');
 var app = express();
 
 // view engine setup
-app.set('port', process.env.PORT || 3001);
+app.set('port', process.env.PORT || 33001);
 app.set('views', path.join(__dirname, 'server/views'));
 app.set('view engine', 'ejs');
 
