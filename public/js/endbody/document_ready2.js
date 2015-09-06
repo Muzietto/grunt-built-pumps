2dfcb28d3324800a135e4ade01bd3fd9a622f04c
Muzietto <marco_faustinelli@yahoo.com>
2015-09-06 17:01:58 +0200

    page 2 comes of age

--- public/js/endbody/document_ready2.js
+++ public/js/endbody/document_ready2.js
@@ -11,16 +11,17 @@ var volumeDown = COMPONENTS.volume(25000, evlevelDown);
 var evBasinDown = WIDGETS.eventedBasin(volumeDown, $('#parentDiv2'), {left:220,bottom:0}).paint();
 var probeDown = WIDGETS.positionalProbe(sensorDown, evBasinDown.domNode(), 120).paint();
 
-// adds water to upper basin
-var pumpIn = WIDGETS.pumpWidget(COMPONENTS.pump(volumeUp, sensorUp, -4500)).init($('#parentDiv2'), {bottom:150,left:20}, 'right').paint();
-// removes water from lower basin
-var pumpOut = WIDGETS.pumpWidget(COMPONENTS.pump(volumeDown, sensorDown, 500)).init($('#parentDiv2'), {bottom:43,left:390}, 'right').paint();
-// connects the two basins
 var flowConn = COMPONENTS.flow(volumeUp, volumeDown, 4000);
-/*
+
+// adds water to upper basin
+var pumpIn = WIDGETS.pumpWidget(COMPONENTS.pump(volumeUp, sensorUp, -1500)).init($('#parentDiv2'), {bottom:150,left:20}, 'right').paint();
+// connects the two basins
+var flowConnPipe = WIDGETS.pipe(flowConn, 'negative').init($('#parentDiv2'), {bottom:5,left:180,width:100,height:110}).paint();
+// removes water from lower basin
+var pumpOut = WIDGETS.pumpWidget(COMPONENTS.pump(volumeDown, sensorDown, 100)).init($('#parentDiv2'), {bottom:43,left:390}, 'right').paint();
+
 var directorId = setInterval(function() {
   pumpIn.onTick();
   pumpOut.onTick();
   flowConn.onTick();
-}, 1);
-*/
+}, 5);
