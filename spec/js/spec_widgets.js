f9bd2301320d34127dbe1b365832cdc00e237349
Muzietto <marco_faustinelli@yahoo.com>
2015-09-07 09:25:56 +0200

    ready to test with jsdom!

--- spec/js/spec_widgets.js
+++ spec/js/spec_widgets.js
@@ -24,14 +24,14 @@ describe('widget', function() {
   beforeEach(function(){ });
   afterEach(function(){ });
 
-  describe('liquidProbe', function() {
+  describe('widget liquidProbe', function() {
     it('gets created with a sensor and a circular template containing a display of the threshold', function() {
 
     });
     it('gets initialized inside the page with a background color', function() {
 
     });
-    describe('once started it', function() {
+    describe('whenever a level_change event is met', function() {
       it('has green bkg when the sensor function returns true', function() {
 
       });
@@ -45,19 +45,36 @@ describe('widget', function() {
     });
   });
 
-  describe('basin', function() {
+  describe('widget pipe', function() {
+    it('gets created with a flow and an incline', function() {
+
+    });
+    it('gets initialized inside the page as a diagonal line along with a display of the flowRate', function() {
+
+    });
+    describe('whenever a level_change event is met', function() {
+      it('gets class \'not_running\' if flow is off', function() {
+
+      });
+      it('gets class \'running\' if flow is on', function() {
+
+      });
+    });
+  });
+
+  describe('widget basin', function() {
     it('gets created with a volume, a scale volume_level/pixels and a rectangular template containing a display of the level', function() {
-      
+
     });
     it('gets initialized inside the page with a width and a height in pixels', function() {
-      
+
     });
-    describe('once started it', function() {
+    describe('whenever a level_change event is met', function() {
       it('increases its height in pixels and updates its display when the volume level increases', function() {
-        
+
       });
       it('decreases its height in pixels and updates its display when the volume level decreases', function() {
-        
+
       });
     });
   });
@@ -66,24 +83,24 @@ describe('widget', function() {
     });
   });
 
-  describe('pumpWidget', function() {
+  describe('widget pumpWidget', function() {
     it('gets created with ...', function() {
-      
+
     });
-    describe('once started it', function() {
-      it('has green bkg when it\' running', function() {
-        
+    describe('whenever a level_change event is met', function() {
+      it('gets green bkg when it\' running', function() {
+
       });
-      it('has red bkg when it is not running', function() {
-        
+      it('gets red bkg when it is not running', function() {
+
       });
     });
   });
-  describe('eventedPumpWidget',function(){
-    it('binds the pump onTick to a global tick event', function() {});
+  describe('widget eventedPumpWidget (not yet existing)',function(){
+    it.skip('binds the pump onTick to a global tick event', function() {});
   });
 
   // pump, basin and two sensors
   describe('feedbackSystem',function(){}); 
-  
+
 }); // end tests
